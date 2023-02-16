72d60a18ced69b32085632d6dbc83ac905bfed07
achozaslopez <122780568+achozaslopez@users.noreply.github.com>
2023-02-16 23:52:33 +0100

    Actualizacion DomainException

--- S4_TiendaSpring/src/main/java/es/rf/tienda/exception/DomainException.java
+++ S4_TiendaSpring/src/main/java/es/rf/tienda/exception/DomainException.java
@@ -2,9 +2,13 @@ package es.rf.tienda.exception;
 
 @SuppressWarnings("serial")
 public class DomainException extends Exception {
-	
-	
-	
-	
+
+	DomainException() {
+
+	} 
+
+	public DomainException(String mensaje) {
+		super("DomainExcepcion : " + mensaje);
+	}
 
 }
