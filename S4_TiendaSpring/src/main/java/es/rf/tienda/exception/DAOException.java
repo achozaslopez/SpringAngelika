5ee12e132b28f732f74289fc786b9e094e0fce57
achozaslopez <122780568+achozaslopez@users.noreply.github.com>
2023-02-16 23:51:51 +0100

    Actualizacion DAOException

--- S4_TiendaSpring/src/main/java/es/rf/tienda/exception/DAOException.java
+++ S4_TiendaSpring/src/main/java/es/rf/tienda/exception/DAOException.java
@@ -1,6 +1,16 @@
 package es.rf.tienda.exception;
 
+@SuppressWarnings("serial")
 public class DAOException extends Exception {
-	
-	
+
+		DAOException() {
+
+		}
+
+		public DAOException(String mensaje) {
+			super("DAOExcepcion : " + mensaje);
+		
+
+	}
+
 }
