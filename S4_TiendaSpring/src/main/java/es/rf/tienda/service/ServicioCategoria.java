316a1fac2f06629301db895355f75a8aafe4744a
achozaslopez <122780568+achozaslopez@users.noreply.github.com>
2023-02-16 23:57:16 +0100

    Actualizacion ServicioCategoria

--- S4_TiendaSpring/src/main/java/es/rf/tienda/service/ServicioCategoria.java
+++ S4_TiendaSpring/src/main/java/es/rf/tienda/service/ServicioCategoria.java
@@ -6,49 +6,79 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import es.rf.tienda.dominio.Categoria;
 import es.rf.tienda.interfaces.daos.ICategoriaDao;
+import es.rf.tienda.dominio.Categoria;
+
 
 @Service
 public class ServicioCategoria implements IServicioCategoria {
-	
+
 	@Autowired
 	private ICategoriaDao cDao;
 	
-	public boolean insert(Categoria t) {
-		if(t.isValidInsert()) {
-		cDao.save(t);
-		return true;
-		}else {
+	public ServicioCategoria(ICategoriaDao cDao) {
+		this.cDao=cDao;
+	}
+	
+	
+
+	public ServicioCategoria() {
+		
+	}
+
+
+
+	public ICategoriaDao getcDao() {
+		return cDao;
+	}
+
+
+
+	public void setcDao(ICategoriaDao cDao) {
+		this.cDao = cDao;
+	}
+
+
+
+	@Override
+	public boolean insert(Categoria c) {
+		if (c.isValidInsert()) {
+			cDao.save(c);
+			return true;
+		} else {
 			return false;
 		}
 	}
-	
-	public boolean update(Categoria t) {
-		if(t.isValidUpdate()) {
-			cDao.save(t);
+
+	@Override
+	public boolean update(Categoria c) {
+		if (c.isValidUpdate()) {
+			cDao.save(c);
 			return true;
-			}else {
-				return false;
-			}
+		} else {
+			return false;
+		}
 	}
-	
-	public boolean deleteById(int s) {
-		cDao.deleteById(s);
-		return true;
-	}
-	
-	public List<Categoria> listAll(){
-		return cDao.findAll();
-	}
-	
-	public Categoria leerUno(int s) {
+
+	@Override
+	public Categoria leerUno(int id) {
 		try {
-			Categoria c = cDao.findById(s).get();
+			Categoria c = cDao.findById(id).get();
 			return c;
-		}catch(NoSuchElementException e) {
+		} catch (NoSuchElementException e) {
 			return new Categoria();
 		}
+	}
+
+	@Override
+	public List<Categoria> listAll() {
+		return cDao.findAll();
+	}
+
+	@Override
+	public void deleteById(int id) {
+		cDao.deleteById(id);
+
 	}
 
 }
